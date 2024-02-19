starting to fix the gradients

school_AI/CostFunctions.py
@@ -2,16 +2,17 @@ import numpy as np
 from Network import Network
 
 def nnCostFunction(net: Network, X: np.ndarray, y: np.ndarray, gama: int = 0.1, addRegTerm=True) -> int:
-
     """
-
-    :param net: the nn
+    this function computes the cost value for the neural network
+    :param net: the network
     :param X: the input data
     :param y: the output data
-    :return: cost value
+    :param gama: the lambda value
+    :param addRegTerm: whether to add the reg term
+    :return: (int) the cost value
     """
 
-    m = X.shape[1]
+    m = X.shape[0]
 
     RegTerm = 0
     Cost = 0

school_AI/Layer.py
@@ -8,24 +8,43 @@ class Layer:
     _numOfNodes = 1
     _scalerFunction = None
     _numOfOutputNodes=1
+    _addA0 = True
 
-    def __init__(self, numOfNodes: int, numOfOutputNodes: int, scalerFunction=Sigmoid, theta: np.ndarray=None):
+    def __init__(self, numOfNodes: int, numOfOutputNodes: int, scalerFunction=Sigmoid, theta: np.ndarray=None, addA0=None):
+        if addA0 is not None:
+            self._addA0 = addA0
         self._numOfOutputNodes = numOfOutputNodes
         self._scalerFunction = scalerFunction
         self._numOfNodes = numOfNodes
         if theta is None:
-            self._theta = np.random.rand(self._numOfNodes, self._numOfOutputNodes)
+            if self._addA0:
+                self._theta = np.random.rand(1 + self._numOfNodes, self._numOfOutputNodes)
+            else:
+                self._theta = np.random.rand(self._numOfNodes, self._numOfOutputNodes)
         else:
             self._theta = theta
 
-    def calcOutput(self, x):
+    def calcOutput(self, x: np.ndarray, addA0=None, applyScaler=True):
+        if addA0 is None:
+            addA0 = self._addA0
+
+        if addA0:
+            # adding a0
+            x = np.append(np.ones((x.shape[0], 1)), x, axis=1)
         output = np.dot(x, self._theta)
 
-        return self._scalerFunction.calculate(output)
+        if applyScaler:
+            return self._scalerFunction.calculate(output)
+        return output
+
+    @property
+    def scaler(self):
+        return self._scalerFunction
 
     def setTheta(self, newTheta: np.ndarray):
         if newTheta.shape != self._theta.shape:
-            raise ValueError("The dimension of the new theta does not match the old shape of theta")
+            raise ValueError("The dimension of the new theta does not match the old shape of theta\n"
+                             f"newTheta: {newTheta.shape} != {self._theta.shape}")
 
         self._theta = copy.deepcopy(newTheta)
 

school_AI/Network.py
@@ -16,15 +16,13 @@ class Network:
     def calculate(self, x):
 
         output = copy.deepcopy(x)
-
         for layer in self._layers:
-            output = layer.calcOutput(x)
-
+            output = layer.calcOutput(output)
 
         return output
 
-    def setTheta(self, Theta: list):
-        for newTheta, layer in zip(Theta, self._layers):
+    def setTheta(self, Thetas: list):
+        for newTheta, layer in zip(Thetas, self._layers):
             layer.setTheta(newTheta)
 
     def getThetas(self):
@@ -36,3 +34,43 @@ class Network:
 
     def getNumOfLabels(self) -> int:
         return self._layers[-1].getNumOfOutputNodes()
+
+    def calculateGradients(self, X, y, gama):
+
+        m = X.shape[0]
+
+        thetas = self.getThetas()
+        thetasGrads = []
+
+        a = []
+        z = []
+
+        for layer in self._layers:
+            X = np.append(np.ones((X.shape[0], 1)), X, axis=1)
+            a.append(X)
+
+            X = layer.calcOutput(X, addA0=False, applyScaler=False)
+            z.append(X)
+
+            X = layer.scaler.calculate(X)
+
+
+        Hypothesis = self.calculate(X)
+
+        deltas = []
+        deltas.insert(0, Hypothesis - y)
+
+        for i in range(len(thetas), 1, -1):
+            print(f"i = {i}")
+            delta = np.dot(self._layers[i].getTheta(), deltas[0])
+            delta = delta * self._layers[i].scaler.getGradient(z[i])
+
+
+        for i in range(len(thetas)):
+            tempGrad = (np.dot(deltas[i].T, a[i])) / m
+            theta = gama * thetas[i]
+            theta[0][0] = 0
+            tempGrad += theta
+            thetasGrads.append(tempGrad)
+
+        return thetasGrads